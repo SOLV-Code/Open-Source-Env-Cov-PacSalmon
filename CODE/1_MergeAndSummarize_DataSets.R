5f51a26226b48c00488f26f1c5173e60c739f145
SOLV-Code <gpestal@solv.ca>
2024-02-13 10:06:17 -0800

    summary table of variables started

--- CODE/1_MergeAndSummarize_DataSets.R
+++ CODE/1_MergeAndSummarize_DataSets.R
@@ -3,6 +3,9 @@
 
 library(tidyverse)
 
+# -------------------------------------------
+# Create merged data set
+# -------------------------------------------
 
 merged.data <- read_csv("DATA/DFO_FraserSockeyeForecast/GENERATED_COVARS_DFOFraserRiverForecasts.csv") %>%
 							full_join(read_csv("DATA/NOAA_OceanConditionsIndex/NOAAOceanCond_DATA.csv",comment = "#"),
@@ -26,4 +29,23 @@ write_lines(comment.text2, merged.filename, append = TRUE)
 write_lines(comment.text3, merged.filename, append = TRUE)
 merged.data |> colnames() |> paste0(collapse = ",") |> write_lines(merged.filename, append = TRUE)
 write_csv(merged.data, merged.filename, append = TRUE)
+
+
+
+# -------------------------------------------
+# Create Summary Table
+# -------------------------------------------
+
+
+var.lookup <- read_csv("DATA/LookUp_VariableDescriptions.csv",comment = "#")
+head(var.lookup)
+
+
+num.obs <- merged.data %>% summarize(across(-Year, ~ sum(!is.na(.x))) ) %>% t() %>% as.data.frame() %>%
+	rownames_to_column("Variable") %>% dplyr::rename(NumObs = V1)
+
+
+var.summary.out <- var.lookup %>% left_join(num.obs, by="Variable")
+write_csv(var.summary.out,"OUTPUT/SummaryOfVariables.csv")
+
 
