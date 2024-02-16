937699df410e70ef1dc84c221a2306b0ee05cb8a
SOLV-Code <gpestal@solv.ca>
2024-02-16 00:33:36 -0800

    pdo comp: calc fixes, plot tweaks

--- CODE/2_PDO_Comparisons.R
+++ CODE/2_PDO_Comparisons.R
@@ -12,6 +12,8 @@ pdo.data <- read_csv("OUTPUT/Merged_CovariateSet.csv",comment="#") %>%
 
 pdo.data
 
+pdo.comp.df <- read_csv("OUTPUT/PDO_Comparisons/PDO_Comparisons_Data.csv")
+pdo.comp.df
 
 # -------------------------------------------
 #  Winter PDO Comparison - Scatter / All Years
@@ -34,13 +36,13 @@ axis(2,at=seq(-6,8,by =2),las=1)
 abline(h=0,col="red",lty=2)
 abline(v=0,col="red",lty=2)
 
-#idx.left.pts <- pdo.data$PDOMeanNovToMar <= -1.2 &
-#										pdo.data$PDOMeanNovToMar > -3
+idx.left.pts <- pdo.data$PDOMeanNovToMar <= -1.5&
+									pdo.data$PDOMeanNovToMar > -3
 
-#text(pdo.data$PDOMeanNovToMar[idx.left.pts],
-#		 pdo.data$PDOSumDecToMarch[idx.left.pts],
-#		 labels = pdo.data$Year[idx.left.pts],
-#		 adj=1.2,col="darkblue",cex=0.8)
+text(pdo.data$PDOMeanNovToMar[idx.left.pts],
+		 pdo.data$PDOSumDecToMarch[idx.left.pts],
+		 labels = pdo.data$Year[idx.left.pts],
+		 adj=1.2,col="darkblue",cex=0.8)
 
 idx.rt.pts <- pdo.data$PDOMeanNovToMar >  1.5
 
@@ -50,7 +52,7 @@ text(pdo.data$PDOMeanNovToMar[idx.rt.pts],
 		 adj=-0.2,col="darkblue",cex=0.8,xpd=NA)
 
 
-idx.top.pts <- pdo.data$PDOSumDecToMarch >  4
+idx.top.pts <- pdo.data$PDOSumDecToMarch >  3
 
 text(pdo.data$PDOMeanNovToMar[idx.top.pts],
 		 pdo.data$PDOSumDecToMarch[idx.top.pts],
@@ -58,7 +60,8 @@ text(pdo.data$PDOMeanNovToMar[idx.top.pts],
 		 adj=-0.2,col="darkblue",cex=0.8,xpd=NA)
 
 
-idx.bt.pts <- pdo.data$PDOSumDecToMarch < -4
+idx.bt.pts <- pdo.data$PDOSumDecToMarch < -4 &
+	pdo.data$PDOMeanNovToMar >= -1.56
 
 text(pdo.data$PDOMeanNovToMar[idx.bt.pts],
 		 pdo.data$PDOSumDecToMarch[idx.bt.pts],
@@ -70,66 +73,13 @@ text(pdo.data$PDOMeanNovToMar[idx.bt.pts],
 dev.off()
 
 
+# quickly check variations
 
+plot(pdo.comp.df$PDOMeanOctToMar,pdo.comp.df$PDOMeanNovToMar)
+plot(pdo.comp.df$PDOMeanDecToMar,pdo.comp.df$PDOMeanNovToMar)
 
-# -------------------------------------------
-#  Winter PDO Comparison - Scatter / Excluding 2023
-# -------------------------------------------
-
-
-png(filename = "OUTPUT/PDO_Comparisons/PDO_Comparisons_Scatter_Excl2023.png",
-		width = 480*4, height = 480*4.2, units = "px",
-		pointsize = 14*3.9, bg = "white",  res = NA)
-par(mai=c(5,5,4,2))
-
-plot(pdo.data$PDOMeanNovToMar,pdo.data$PDOSumDecToMarch,
-		 xlim=c(-3,3),
-		 xlab = "PDO Mean Nov-Mar\n(DFO Fraser FC)",
-		 ylab = "PDO Sum Dec-Mar\n(NOAA Ocean Cond Index)",
-		 bty="n",axes=FALSE,pch=21,col="darkblue",bg="lightblue",
-		 cex=1.3,
-		 main="Comparison of Winter PDO Covariates\nExcluding 2022,2023")
-axis(1)
-axis(2,at=seq(-6,8,by =2),las=1)
-abline(h=0,col="red",lty=2)
-abline(v=0,col="red",lty=2)
-
-#idx.left.pts <- pdo.data$PDOMeanNovToMar <= -1.2 &
-#	pdo.data$PDOMeanNovToMar > -3
-
-#text(pdo.data$PDOMeanNovToMar[idx.left.pts],
-#		 pdo.data$PDOSumDecToMarch[idx.left.pts],
-#		 labels = pdo.data$Year[idx.left.pts],
-#		 adj=1.2,col="darkblue",cex=0.8)
-
-idx.rt.pts <- pdo.data$PDOMeanNovToMar >  1.5
-
-text(pdo.data$PDOMeanNovToMar[idx.rt.pts],
-		 pdo.data$PDOSumDecToMarch[idx.rt.pts],
-		 labels = pdo.data$Year[idx.rt.pts],
-		 adj=-0.2,col="darkblue",cex=0.8,xpd=NA)
-
-
-idx.top.pts <- pdo.data$PDOSumDecToMarch >  4
-
-text(pdo.data$PDOMeanNovToMar[idx.top.pts],
-		 pdo.data$PDOSumDecToMarch[idx.top.pts],
-		 labels = pdo.data$Year[idx.top.pts],
-		 adj=-0.2,col="darkblue",cex=0.8,xpd=NA)
-
-
-idx.bt.pts <- pdo.data$PDOSumDecToMarch < -4
-
-text(pdo.data$PDOMeanNovToMar[idx.bt.pts],
-		 pdo.data$PDOSumDecToMarch[idx.bt.pts],
-		 labels = pdo.data$Year[idx.bt.pts],
-		 adj=-0.2,col="darkblue",cex=0.8,xpd=NA)
-
-
-
-dev.off()
-
-
+plot(pdo.comp.df$PDOMeanOctToMar,pdo.comp.df$PDOSumNovToMar)
+plot(pdo.comp.df$PDOMeanDecToMar,pdo.comp.df$PDOSumNovToMar)
 
 
 # -------------------------------------------
