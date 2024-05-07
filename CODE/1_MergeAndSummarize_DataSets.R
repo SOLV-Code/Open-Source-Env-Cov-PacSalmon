5b6db08fa6a030d034a8309fd9cf8bb26a123e1a
SOLV-Code <gpestal@solv.ca>
2024-05-07 09:47:02 -0700

    readme edits: acronym

--- CODE/1_MergeAndSummarize_DataSets.R
+++ CODE/1_MergeAndSummarize_DataSets.R
@@ -23,7 +23,7 @@ head(merged.data)
 # as per https://stackoverflow.com/questions/72304594/added-commented-section-to-output-csv-with-write-csv
 merged.filename <- "OUTPUT/Merged_CovariateSet.csv"
 
-comment.text1 <- paste("# Merged set of all available records in the OSEC-PS repository")
+comment.text1 <- paste("# Merged set of all available records in the PSECrepository")
 comment.text2 <- paste("# Variable descriptions and source data available at https://github.com/SOLV-Code/Open-Source-Env-Cov-PacSalmon")
 comment.text3 <- paste("# File generated on", format(Sys.time(), "%a %b %e %H:%M:%S %Y"))
 
