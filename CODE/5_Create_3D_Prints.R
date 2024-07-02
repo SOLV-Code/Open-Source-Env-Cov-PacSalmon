903f6d935dd995a1dc0aaa3456e8c647bc80769f
SOLV-Code <30556192+SOLV-Code@users.noreply.github.com>
2024-07-02 13:20:53 -0700

    npgo and pdo shapefiles done, *.stl added to gitignore

--- CODE/5_Create_3D_Prints.R
+++ CODE/5_Create_3D_Prints.R
@@ -7,6 +7,7 @@ library(pacea) # use this as the data source
 
 # get custom functions from the FigRs repo at https://github.com/SOLV-Code/FigRs
 # 3D printing code and steps are summarized here: https://github.com/SOLV-Code/FigRs/tree/master/3D%20Prints
+# the stl files generated here can be post-processed using Blender as per the tips there
 
 source("https://raw.githubusercontent.com/SOLV-Code/FigRs/master/3D%20Prints/Time%20Series/FUNCTION_r2stlMOD.R")
 source("https://raw.githubusercontent.com/SOLV-Code/FigRs/master/3D%20Prints/Time%20Series/FUNCTION_ts2persp.R")
@@ -16,7 +17,7 @@ source("https://raw.githubusercontent.com/SOLV-Code/FigRs/master/3D%20Prints/Tim
 if(!dir.exists("OUTPUT/SourcesFor3DPrints")){dir.create("OUTPUT/SourcesFor3DPrints")}
 
 
-
+# ----------------------------------------------------------------------------------
 # Smoothed PDO series
 start.yr <- 1980
 end.yr <- 2023
@@ -58,3 +59,53 @@ r2stl.mod(x=c(1:dim(pdo_persp)[1])/scalar,
 					filename="OUTPUT/SourcesFor3DPrints/PDO_SmoothedAnomalies_1980to2023.stl",
 					show.persp=TRUE,z.expand=TRUE,
 					min.height=0.008)
+
+
+
+# ----------------------------------------------------------------------------------
+# Smoothed NPGO series
+start.yr <- 1980
+end.yr <- 2023
+smoother.span <- 1/35 # f value for lowess()
+
+npgo.ts <- ts(npgo %>% dplyr::filter(year>=start.yr, year <= end.yr) %>% select(anomaly) ,start = c(start.yr,1),freq=12)
+npgo.ts
+npgo.lowess <- lowess(npgo.ts,f=smoother.span)
+npgo.lowess
+plot(npgo.lowess, type="l", col="dodgerblue", bty="n",axes=TRUE,xlab="Year", ylab="NPGO Anomaly (Lowess Smoothed)")
+abline(h=0,col="red")
+
+# use only first smoothed value from each year
+# to work with default dimension of generated surfaces
+jan.idx <- c(TRUE,rep(FALSE,11))
+lines(npgo.lowess$x[jan.idx],npgo.lowess$y[jan.idx], col="red")
+
+
+# change to difference from lowest value (plus a base of 5%) and
+npgo.lowess.adj <- npgo.lowess$y[jan.idx] - min(0.95* npgo.lowess$y[jan.idx])
+npgo.lowess.adj
+
+#rescale to a max height of some specified units relative to the 4 grid cells/year
+npgo.lowess.adj <- npgo.lowess.adj/max(npgo.lowess.adj)*80
+
+# convert time series to surface coordinates
+npgo_persp <- ts2persp(npgo.lowess.adj)
+
+# plot surface
+persp(x=1:dim(npgo_persp)[1],y=1:dim(npgo_persp)[2],z=npgo_persp,theta=90,phi=25, zlim=c(0,max(npgo_persp)), scale=FALSE)
+
+# scale to determine size of eventual object (2 = half of default size)
+scalar <- 1
+
+# create stl file
+r2stl.mod(x=c(1:dim(npgo_persp)[1])/scalar,
+					y=c(1:dim(npgo_persp)[2]), #/scalar,
+					z=npgo_persp/scalar,
+					filename="OUTPUT/SourcesFor3DPrints/NPGO_SmoothedAnomalies_1980to2023.stl",
+					show.persp=TRUE,z.expand=TRUE,
+					min.height=0.008)
+
+
+
+
+
