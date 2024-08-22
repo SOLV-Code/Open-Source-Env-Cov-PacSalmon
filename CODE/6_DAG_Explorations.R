5061f2029650fc995f65aa6c0b8bd37d58eaf67d
SOLV-Code <30556192+SOLV-Code@users.noreply.github.com>
2024-08-22 16:11:59 -0700

    DAG software exploration

--- CODE/6_DAG_Explorations.R
+++ CODE/6_DAG_Explorations.R
@@ -13,6 +13,8 @@ library(ggplot2)
 # Basic SR
 dag.basic.sr <- dagify(
 	Rec ~ Spn,
+	Rec ~ Prod,
+	Prod ~ Spn,
 	exposure = "Spn",
 	outcome = "Rec"
 ) %>%
@@ -20,10 +22,12 @@ dag.basic.sr <- dagify(
 
 ggdag(dag.basic.sr) + theme_dag()
 
+
 # 2 stage life-history SR
 dag.2stages.sr <- dagify(
-	Rec ~ Juv,
-	Juv ~ Spn,
+	Rec ~ Juv + Mp,
+	Juv ~ Spn + FWp,
+	FWp ~ Spn,
 	exposure = "Spn",
 	outcome = "Rec"
 ) %>%
@@ -31,18 +35,47 @@ dag.2stages.sr <- dagify(
 
 ggdag(dag.2stages.sr) + theme_dag()
 
+
+########
+# dagitty code
+
+dag {
+	bb="0,0,1,1"
+	ONIAnomWinterY2 [pos="0.201,0.210"]
+	OceanCondY2 [latent,pos="0.300,0.271"]
+	OceanCondY3 [pos="0.418,0.266"]
+	PDOAnomWinterY2 [pos="0.198,0.289"]
+	Prod [pos="0.377,0.388"]
+	Rec [outcome,pos="0.564,0.490"]
+	Spn [exposure,pos="0.217,0.499"]
+	OceanCondY2 -> ONIAnomWinterY2
+	OceanCondY2 -> PDOAnomWinterY2
+	OceanCondY2 -> Prod
+	OceanCondY3 -> Prod
+	Prod -> Rec
+	Spn -> Prod
+	Spn -> Rec
+}
+
+
+
+
+
+
+
+dag.2stages.sr %>%
+	ggplot(aes(x = x, y = y, xend = xend, yend = yend)) +
+	geom_dag_point(color = "orange") +
+	geom_dag_edges_arc(edge_color = "blue", curvature = 0) +
+	geom_dag_text(color = "black") +
+	theme_dag()
+
+
+# https://stackoverflow.com/questions/76653868/use-labels-with-ggdag-and-ggplot
+#https://stackoverflow.com/questions/71441472/change-color-for-specific-nodes-in-ggdag/71441773#71441773
+
+
 # 2 stage life-history SR
-
-dag.3stages.sr <- dagify(
-	Rec ~ Juv,
-	Juv ~ Eggs,
-	Eggs ~ Spn,
-	exposure = "Spn",
-	outcome = "Rec"
-) %>%
-	tidy_dagitty()
-
-ggdag(dag.3stages.sr) + theme_dag()
 
 
 
